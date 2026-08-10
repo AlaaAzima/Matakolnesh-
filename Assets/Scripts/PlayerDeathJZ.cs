cae857b7ce8d344c879e63e98a7757bee6fce20f
AlaaAzima <alaaazima632006@gmail.com>
2026-08-10 12:41:27 +0300

    Spoon Asssets 2

--- Assets/Scripts/PlayerDeathJZ.cs
+++ Assets/Scripts/PlayerDeathJZ.cs
@@ -1,15 +1,50 @@
 using UnityEngine;
 
-public class PlayerDeathJZ : MonoBehaviour,IDeath
+public class PlayerDeathJZ : MonoBehaviour, IDeath
 {
-    bool isDead = false;
+    [SerializeField] private float destroyDelay = 1.1f;
+
+    private Animator animator;
+    private Collider2D playerCollider;
+    private Rigidbody2D rb;
+    private bool isDead = false;
+
+
+    private static readonly int DieTriggerHash = Animator.StringToHash("IsDead");
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        playerCollider = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     public void Die()
     {
         if (isDead) return;
         isDead = true;
 
-        GameManagerJE.Instance.PlayerDied();
-        Destroy(gameObject);
 
+        if (GameManagerJE.Instance != null)
+        {
+            GameManagerJE.Instance.PlayerDied();
+        }
+
+
+        if (animator != null)
+        {
+            animator.SetTrigger(DieTriggerHash);
+        }
+
+
+        if (playerCollider != null) playerCollider.enabled = false;
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector2.zero;
+            rb.bodyType = RigidbodyType2D.Kinematic;
+        }
+
+
+        Destroy(gameObject, destroyDelay);
     }
 }
