51d5bc51d2f5e1db9421ed17e44fc93606a7073d
AlaaAzima <alaaazima632006@gmail.com>
2026-08-13 02:30:57 +0300

    Make Player Attack

---
+++ Assets/Scripts/NewPlayer/ArcherAim.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class ArcherAim : MonoBehaviour
+{
+
+    public Sprite[] bowSprites;
+
+
+    public float minAngle = -45f;
+    public float maxAngle = 45f;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        AimAtMouse();
+    }
+
+    void AimAtMouse()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - transform.position;
+
+
+        if (direction.x < 0)
+        {
+            spriteRenderer.flipX = true;
+            direction.x = -direction.x;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float clampedAngle = Mathf.Clamp(angle, minAngle, maxAngle);
+        float t = Mathf.InverseLerp(minAngle, maxAngle, clampedAngle);
+
+        int index = Mathf.RoundToInt(t * (bowSprites.Length - 1));
+
+        spriteRenderer.sprite = bowSprites[index];
+    }
+}
