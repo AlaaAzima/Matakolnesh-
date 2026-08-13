e292518b43f611b95d97dcd3a8a8d8dc6fc0f0d8
janaeldereny <janaeldereny@gmail.com>
2026-08-13 12:49:28 +0300

    some edits on lvl17

--- Assets/Scripts/ButtonWire/PushedBall.cs
+++ Assets/Scripts/ButtonWire/PushedBall.cs
@@ -5,15 +5,14 @@ public class PushedBall : MonoBehaviour
     private bool isStuck = false;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Collision with: " + collision.gameObject.name);
+        
         if (isStuck) return;
         if (collision.collider.TryGetComponent<IDeath>(out IDeath ideath))
         {
+            Debug.Log("IDeath found!");
             ideath.Die();
         }
     }
-
-
-
-
 
 }
