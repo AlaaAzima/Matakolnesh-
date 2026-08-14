9dabc947bb98dc1a8b7a59b79a7dba21dfadcfc8
janaeldereny <janaeldereny@gmail.com>
2026-08-15 02:50:43 +0300

    buttom and stars pop up

--- Assets/Scripts/WinPanelAnimJE.cs
+++ Assets/Scripts/WinPanelAnimJE.cs
@@ -2,112 +2,162 @@ using UnityEngine;
 
 public class WinPanelAnimJE : MonoBehaviour
 {
-       [SerializeField] private RectTransform[] stars;
+    [SerializeField] private RectTransform[] stars;
+
+    [Header("Star Animation Settings")]
+    [SerializeField] private float emptyStarScale = 0.5f;      // placeholder scale for unearned stars
+    [SerializeField] private float earnedStarPopScale = 0.6f;  // overshoot scale on pop-in
+    [SerializeField] private float earnedStarSettleScale = 0.5f;
+    [SerializeField] private float starPopDuration = 0.2f;
+    [SerializeField] private float starSettleDuration = 0.1f;
+    [SerializeField] private float starDelayStep = 0.15f;
 
     private RectTransform winPanel;
 
     private void Awake()
-
     {
-
         winPanel = GetComponent<RectTransform>();
-
     }
-
-private void PlayStarsAnimation(int earnedStars)
-{
-    for (int i = 0; i < stars.Length; i++)
-    {
-        int index = i;
-
-        // كل النجوم تبدأ من صفر
-        stars[index].localScale = Vector3.zero;
-
-        if (index < earnedStars)
-        {
-            LeanTween.scale(
-                stars[index].gameObject,
-                Vector3.one * 0.6f,
-                0.2f
-            )
-            .setDelay(index * 0.15f)
-            .setIgnoreTimeScale(true)
-            .setEaseOutQuad()
-            .setOnComplete(() =>
-            {
-                LeanTween.scale(
-                    stars[index].gameObject,
-                    Vector3.one * 0.5f,
-                    0.1f
-                )
-                .setIgnoreTimeScale(true)
-                .setEaseOutQuad();
-            });
-        }
-        else
-        {
-            // النجوم غير المكتسبة تظهر كـ outline
-            stars[index].localScale = Vector3.one * 0.5f;
-        }
-    }
-}
 
     public void PlayAnimation(int earnedStars)
-
     {
-
-        // Panel يبدأ صغير
+       
+        LeanTween.cancel(winPanel.gameObject);
+        foreach (var star in stars)
+        {
+            if (star != null) LeanTween.cancel(star.gameObject);
+        }
 
         winPanel.localScale = Vector3.zero;
 
-        // كل النجوم تبدأ ظاهرة
-
         for (int i = 0; i < stars.Length; i++)
-
         {
-
-            stars[i].localScale = Vector3.one;
-
+            stars[i].localScale = (i < earnedStars)
+                ? Vector3.zero
+                : Vector3.one * emptyStarScale;
         }
 
-        // Panel Animation
-
         LeanTween.scale(winPanel, Vector3.one, 0.5f)
-
             .setIgnoreTimeScale(true)
-
             .setEaseOutBack()
-
             .setOnComplete(() => PlayStarsAnimation(earnedStars));
-
     }
 
-    // private void PlayStarsAnimation(int earnedStars)
+    private void PlayStarsAnimation(int earnedStars)
+    {
+        for (int i = 0; i < stars.Length; i++)
+        {
+            if (i >= earnedStars) continue; 
 
-    // {
+            int index = i;
 
-    //     for (int i = 0; i < stars.Length; i++)
-
-    //     {
-
-    //         if (i < earnedStars)
-
-    //         {
-
-    //             // النجمة المكتسبة تبدأ من صفر
-
-    //             stars[i].localScale = Vector3.zero;
-
-    //             // Pop Animation
-
-    //             LeanTween.scale(stars[i].gameObject, Vector3.one, 0.25f)
-    //             .setDelay(i * 0.15f)
-    //             .setIgnoreTimeScale(true)
-    //             .setEaseOutQuad();
-    //         }
-
-    //     }
-
-    // }
-
+            LeanTween.scale(stars[index].gameObject, Vector3.one * earnedStarPopScale, starPopDuration)
+                .setDelay(index * starDelayStep)
+                .setIgnoreTimeScale(true)
+                .setEaseOutQuad()
+                .setOnComplete(() =>
+                {
+                    LeanTween.scale(stars[index].gameObject, Vector3.one * earnedStarSettleScale, starSettleDuration)
+                        .setIgnoreTimeScale(true)
+                        .setEaseOutQuad();
+                });
+        }
+    }
 }
+
+
+// using UnityEngine;
+
+// public class WinPanelAnimJE : MonoBehaviour
+// {
+//        [SerializeField] private RectTransform[] stars;
+
+//     private RectTransform winPanel;
+
+//     private void Awake()
+
+//     {
+
+//         winPanel = GetComponent<RectTransform>();
+
+//     }
+
+//   private void PlayStarsAnimation(int earnedStars)
+// {
+//     for (int i = 0; i < stars.Length; i++)
+//     {
+//         int index = i;
+
+//         if (index < earnedStars)
+//         {
+//             stars[index].localScale = Vector3.zero;
+
+//             LeanTween.scale(stars[index].gameObject, Vector3.one * 0.6f, 0.2f)
+//                 .setDelay(index * 0.15f)
+//                 .setIgnoreTimeScale(true)
+//                 .setEaseOutQuad()
+//                 .setOnComplete(() =>
+//                 {
+//                     LeanTween.scale(stars[index].gameObject, Vector3.one * 0.5f, 0.1f)
+//                         .setIgnoreTimeScale(true)
+//                         .setEaseOutQuad();
+//                 });
+//         }
+//         else
+//         {
+//             stars[index].localScale = Vector3.one * 0.5f;
+//         }
+//     }
+// }
+
+//     public void PlayAnimation(int earnedStars)
+
+//     {
+//         winPanel.localScale = Vector3.zero;
+//         for (int i = 0; i < stars.Length; i++)
+
+//         {
+
+//             stars[i].localScale = Vector3.one;
+
+//         }
+
+//         LeanTween.scale(winPanel, Vector3.one, 0.5f)
+
+//             .setIgnoreTimeScale(true)
+
+//             .setEaseOutBack()
+
+//             .setOnComplete(() => PlayStarsAnimation(earnedStars));
+
+//     }
+
+//     // private void PlayStarsAnimation(int earnedStars)
+
+//     // {
+
+//     //     for (int i = 0; i < stars.Length; i++)
+
+//     //     {
+
+//     //         if (i < earnedStars)
+
+//     //         {
+
+//     //             // النجمة المكتسبة تبدأ من صفر
+
+//     //             stars[i].localScale = Vector3.zero;
+
+//     //             // Pop Animation
+
+//     //             LeanTween.scale(stars[i].gameObject, Vector3.one, 0.25f)
+//     //             .setDelay(i * 0.15f)
+//     //             .setIgnoreTimeScale(true)
+//     //             .setEaseOutQuad();
+//     //         }
+
+//     //     }
+
+//     // }
+
+// }
