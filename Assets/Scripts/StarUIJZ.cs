d1de1ffb4fe5dbab1c7216d01b826c77259cc551
AlaaAzima <alaaazima632006@gmail.com>
2026-08-15 20:26:36 +0300

    update win pannel

--- Assets/Scripts/StarUIJZ.cs
+++ Assets/Scripts/StarUIJZ.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 public class StarUIJZ : MonoBehaviour
 {
     [SerializeField] private Image[] stars;
     [SerializeField] private Sprite fullStar;
     [SerializeField] private Sprite emptyStar;
 
+
+    [SerializeField] private ParticleSystem[] starParticles;
+
     public void DisplayStars(int earnedStars)
     {
         for (int i = 0; i < stars.Length; i++)
         {
-            stars[i].sprite = (i < earnedStars) ? fullStar : emptyStar;
+            bool isEarned = (i < earnedStars);
+            stars[i].sprite = isEarned ? fullStar : emptyStar;
+
+
+            if (isEarned && starParticles != null && i < starParticles.Length)
+            {
+                if (starParticles[i] != null)
+                {
+                    starParticles[i].Play();
+                }
+            }
         }
     }
 }
