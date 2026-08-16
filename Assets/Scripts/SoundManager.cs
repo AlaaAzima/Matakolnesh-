bef6d1d93299158ce31cea25266daf7bd7e36c74
janaeldereny <janaeldereny@gmail.com>
2026-08-16 09:58:53 +0300

    music and sfx button

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -37,6 +37,10 @@ public class SoundManager : MonoBehaviour
     private bool musicStopped = false;
     private bool trackStarted = false;
 
+    private bool sfxMuted = false;
+private float previousSFXVolume = 1f;
+
+
     private void Awake()
     {
         if (Instance != null)
@@ -172,6 +176,74 @@ public class SoundManager : MonoBehaviour
         Instance.playlistActive = true;
         Instance.PlayCurrentTrack();
     }
+
+//     public static void ToggleMusicMute()
+// {
+//     if (Instance == null) return;
+
+//     Instance.musicMuted = !Instance.musicMuted;
+
+//     if (Instance.musicMuted)
+//     {
+//         Instance.previousMusicVolume = Instance.musicSource.volume;
+//         Instance.musicSource.volume = 0f;
+//     }
+//     else
+//     {
+//         Instance.musicSource.volume = Instance.previousMusicVolume;
+//     }
+// }
+
+// public static bool IsMusicMuted()
+// {
+//     if (Instance == null) return false;
+
+//     return Instance.musicMuted;
+// }
+
+
+public static void ToggleSFXMute()
+{
+    if (Instance == null) return;
+
+    Instance.sfxMuted = !Instance.sfxMuted;
+
+    if (Instance.sfxMuted)
+    {
+        Instance.previousSFXVolume = Instance.sfxSource.volume;
+        Instance.sfxSource.volume = 0f;
+    }
+    else
+    {
+        Instance.sfxSource.volume = Instance.previousSFXVolume;
+    }
+}
+
+public static bool IsSFXMuted()
+{
+    if (Instance == null) return false;
+
+    return Instance.sfxMuted;
+}
+
+
+private bool musicMuted = false;
+
+public static void ToggleMusicMute()
+{
+    if (Instance == null) return;
+
+    Instance.musicMuted = !Instance.musicMuted;
+
+    Instance.musicSource.mute = Instance.musicMuted;
+}
+
+public static bool IsMusicMuted()
+{
+    if (Instance == null) return false;
+
+    return Instance.musicMuted;
+}
 
 
 
