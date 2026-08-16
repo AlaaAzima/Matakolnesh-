c7c8397fe8caaeb42e6af15b8f7727fd38d1f61b
AlaaAzima <alaaazima632006@gmail.com>
2026-08-16 13:56:16 +0300

    Button Animation

--- Assets/Scripts/ButtonWire/ButtonJZ.cs
+++ Assets/Scripts/ButtonWire/ButtonJZ.cs
@@ -1,10 +1,30 @@
 using UnityEngine;
 using System;
-public class ButtonJZ : MonoBehaviour,IInteractable
+
+public class ButtonJZ : MonoBehaviour, IInteractable
 {
     public event Action OnButtonClick;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private string clickTriggerName = "IsClicked";
+
+    private void Awake()
+    {
+
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
+
     public void Interact()
     {
+
+        if (animator != null)
+        {
+            animator.SetTrigger(clickTriggerName);
+        }
+
         OnButtonClick?.Invoke();
     }
 }
